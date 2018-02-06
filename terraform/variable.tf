fe54f1004cf1444294b3f0445727392b4186e95d
mickey.shaul@gmail.com <mickey.shaul@gmail.com>
2018-02-06 20:13:33 +0200

    first commit to cassandra automation

---
+++ terraform/variable.tf
@@ -0,0 +1,34 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable default_keypair_name {
+  description = "Name of the KeyPair used for all nodes"
+  default = "Opsschool-1"
+}
+variable instance_type {
+  default = "t2.medium"
+}
+
+variable cassandra_servers {
+  default = "3"
+}
+
+variable owner {
+  default = "Cassandra"
+}
+
+variable vpc_id {
+  default = "vpc-584d6e20"
+}
+
+variable cassandra_cluster {
+  default = "opsschool"
+}
+
+variable user {
+  default = "ubuntu"
+}
